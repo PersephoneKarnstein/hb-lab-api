fef656d5fdc3077f04c48ee1945c1e134dd05d0c
Hackbright Student <no-reply@hackbrightacademy.com>
2019-04-25 16:55:23 -0700

    update gitignore

--- secrets.sh
+++ secrets.sh
@@ -2,6 +2,6 @@
 # Find these at: https://www.eventbrite.ca/account-settings/apps
 # This file should never go into github!
 
-export EVENTBRITE_KEY="Your-Key-Goes-Here"
-export EVENTBRITE_SECRET="Your-Client-Secret-Go-Here"
-export EVENTBRITE_TOKEN="Your-Personal-OAuth-Token-Goes-Here"
+export EVENTBRITE_KEY="2ORTOI2EUV22COZ6C4"
+export EVENTBRITE_SECRET="AH634UINKQVGJQB3LGMPCVEVDY5OC6VJWHDKE2QPWBFDSM4MGW"
+export EVENTBRITE_TOKEN="OQ53JZZX727S4AOO43A3"
